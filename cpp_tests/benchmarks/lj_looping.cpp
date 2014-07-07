759138f105c9c17bc7892e7f193520ac1c3b4716
Jacob Stevenson <js850@cam.ac.uk>
2014-07-07 15:17:48 +0100

    clean up benchmarks a bit

--- cpp_tests/benchmarks/lj_looping.cpp
+++ cpp_tests/benchmarks/lj_looping.cpp
@@ -13,8 +13,7 @@ using pele::BasePotential;
 using pele::lj_interaction;
 using pele::cartesian_distance;
 
-template<typename pairwise_interaction,
-             typename distance_policy = cartesian_distance<3>>
+template<typename pairwise_interaction, typename distance_policy = cartesian_distance<3>>
 class SimplePairwisePotential1 : public BasePotential
 {
 public:
@@ -29,6 +28,51 @@ public:
     {}
 
     inline double get_energy_gradient(Array<double> x, Array<double> grad)
+    {
+        double e=0.;
+        double gij;
+        double dr[_ndim];
+        const size_t natoms = x.size() / _ndim;
+
+        grad.assign(0.);
+
+        for(size_t atomi=0; atomi<natoms; ++atomi) {
+            int const i1 = _ndim*atomi;
+            for(size_t atomj=0; atomj<atomi; ++atomj) {
+                int j1 = _ndim*atomj;
+
+                _dist->get_rij(dr, &x[i1], &x[j1]);
+
+                double r2 = 0;
+                for (size_t k=0; k<_ndim; ++k) {
+                    r2 += dr[k]*dr[k];
+                }
+                e += _interaction->energy_gradient(r2, &gij, atomi, atomj);
+                for(size_t k=0; k<_ndim; ++k)
+                    grad[i1+k] -= gij * dr[k];
+                for(size_t k=0; k<_ndim; ++k)
+                    grad[j1+k] += gij * dr[k];
+            }
+        }
+        return e;
+    }
+};
+
+template<typename pairwise_interaction, typename distance_policy = cartesian_distance<3>>
+class SPP2 : public BasePotential
+{
+public:
+    static const size_t _ndim = distance_policy::_ndim;
+    std::shared_ptr<pairwise_interaction> _interaction;
+    std::shared_ptr<distance_policy> _dist;
+
+    SPP2(
+            std::shared_ptr<pairwise_interaction> interaction,
+            std::shared_ptr<distance_policy> dist=NULL)
+        : _interaction(interaction), _dist(dist)
+    {}
+
+    double get_energy_gradient(Array<double> x, Array<double> grad)
     {
         double e=0.;
         double gij;
@@ -57,6 +101,15 @@ public:
     }
 };
 
+void bench(pele::BasePotential * pot, Array<double> x, size_t const N=50000)
+{
+    auto g = x.copy();
+    clock_t t0 = clock();
+    for (size_t i = 0; i<N; ++i){
+        pot->get_energy_gradient(x, g);
+    }
+    cout << double(clock() - t0) / CLOCKS_PER_SEC << "\n";
+}
 
 int main()
 {
@@ -72,17 +125,15 @@ int main()
     lbfgs.run();
     x= lbfgs.get_x();
 
-    auto g = x.copy();
-    clock_t t0 = clock();
-    for (size_t i = 0; i<1000; ++i){
-        lj.get_energy_gradient(x, g);
-    }
-    cout << double(clock() - t0) / CLOCKS_PER_SEC << "\n";
+    bench(&lj, x);
 
-    auto pot1 = SimplePairwisePotential1<lj_interaction, cartesian_distance<3> >(
-            std::make_shared<lj_interaction>(1., 1.), make_shared<cartesian_distance<3> >());
-    for (size_t i = 0; i<1000; ++i){
-        pot1.get_energy_gradient(x, g);
-    }
+    auto pot1 = SimplePairwisePotential1<lj_interaction>(
+            std::make_shared<lj_interaction>(1., 1.));
+    bench(&pot1, x);
+
+    auto pot2 = SPP2<lj_interaction>(
+            std::make_shared<lj_interaction>(1., 1.));
+    bench(&pot2, x);
+
 
 }
